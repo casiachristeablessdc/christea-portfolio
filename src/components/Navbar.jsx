9b04ed987c29b509de253e0924916cc9f2e77176
CPE3B-casia-christeabless <2022106704@ms.bulsu.edu.ph>
2026-07-08 23:23:40 +0800

    Add navigation bar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,16 @@
+function Navbar() {
+  return (
+    <nav className="navbar">
+      <h2 className="logo">Christea</h2>
+
+      <ul className="nav-links">
+        <li>About</li>
+        <li>Experience</li>
+        <li>Projects</li>
+        <li>Contact</li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Navbar;
