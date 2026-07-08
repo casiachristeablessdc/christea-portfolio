c800b0f8ae986b37bd02f2e65f59a403f561c075
CPE3B-casia-christeabless <2022106704@ms.bulsu.edu.ph>
2026-07-08 22:33:13 +0800

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div>
+      <h1>Christea Bless DC. Casia</h1>
+      <p>Computer Engineering Student</p>
+      <p>Portfolio Website Coming Soon 🚀</p>
+    </div>
+  );
+}
+
+export default App;
