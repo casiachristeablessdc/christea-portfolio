5373c6de3cab9fd16d2ec352065dbcef47cea61d
CPE3B-casia-christeabless <2022106704@ms.bulsu.edu.ph>
2026-07-09 15:20:36 +0800

    Set up foundation layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
+import Navbar from "./components/Navbar";
+
 function App() {
   return (
-    <main>
-      <h1>Portfolio Under Construction</h1>
-      <p>Building my portfolio with React and Vite.</p>
-    </main>
+    <>
+      <Navbar />
+
+      <main>
+        <h1>Portfolio Under Construction</h1>
+        <p>Building my portfolio with React and Vite.</p>
+      </main>
+    </>
   );
 }
 
